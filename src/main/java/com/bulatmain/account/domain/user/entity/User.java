2865c5cace84ac3a650ca429e0cbaf8f4b52872d
bulat <sinntexxx@gmail.com>
2024-11-17 21:05:12 +0300

    Created User entity

---
+++ src/main/java/com/bulatmain/account/domain/user/entity/User.java
@@ -0,0 +1,14 @@
+package com.bulatmain.account.domain.user.entity;
+
+import com.bulatmain.account.domain.user.value.Id;
+import com.bulatmain.account.domain.user.value.Login;
+import com.bulatmain.account.domain.user.value.Password;
+import com.bulatmain.account.domain.user.value.detail.UserDetail;
+
+public class User {
+    private Id id;
+    private Login login;
+    private Password password;
+    private UserDetail detail;
+
+}
