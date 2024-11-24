fb0cb5c6e8aba43c0f97545377d7d8321a8a0720
bulat <sinntexxx@gmail.com>
2024-11-24 17:30:49 +0300

    Refactored SecurityConfig with deprecated methdos

--- src/main/java/com/bulatmain/account/config/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/bulatmain/account/config/infrastructure/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.bulatmain.account.config.infrastructure.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,9 +12,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable() // Disable CSRF for development
-                .authorizeRequests()
-                .anyRequest().permitAll(); // Allow all requests without authentication
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                );
         return http.build();
     }
 }
