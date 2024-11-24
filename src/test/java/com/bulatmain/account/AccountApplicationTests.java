f93c05e17e8d6be0e70bf2664b150d930e1ed643
bulat <sinntexxx@gmail.com>
2024-11-24 17:31:44 +0300

    dockerized app

--- src/test/java/com/bulatmain/account/AccountApplicationTests.java
+++ src/test/java/com/bulatmain/account/AccountApplicationTests.java
@@ -2,8 +2,10 @@ package com.bulatmain.account;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:test.properties")
 class AccountApplicationTests {
 
 	@Test
