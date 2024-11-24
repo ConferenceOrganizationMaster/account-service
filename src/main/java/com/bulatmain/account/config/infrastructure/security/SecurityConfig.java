9a9f945aa499c63dd41e1d32e0734c2efa990f5e
bulat <sinntexxx@gmail.com>
2024-11-24 14:31:07 +0300

    added SecurityConfig for disabling CSRF filter at dev

---
+++ src/main/java/com/bulatmain/account/config/infrastructure/security/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.bulatmain.account.config.infrastructure.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable() // Disable CSRF for development
+                .authorizeRequests()
+                .anyRequest().permitAll(); // Allow all requests without authentication
+        return http.build();
+    }
+}
+
